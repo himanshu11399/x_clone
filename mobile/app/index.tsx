af42bb4c162afedee5d20d73b632f10222c8bf7d
Himanshu Sharma <himanshusharma12283@gmail.com>
2025-10-07 04:36:45 +0530

    Initialize tailwind

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,15 +1,23 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import {Ionicons} from "@expo/vector-icons"
+import "../global.css"
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+   <View className="flex-1 items-center justify-center bg-gray-500">
+      <Text className="text-xl font-bold text-blue-500 ">
+        Welcome to Nativewind!
+      </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#cebbbbff"
+
+  }
+})
